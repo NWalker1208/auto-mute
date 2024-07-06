9cd2d6ed3a163c22f5f322fc78ebe8329900eede
Nathan Walker <nathanwalker1208@gmail.com>
2024-07-06 11:59:56 -0600

    Create Maven project

---
+++ automute/src/main/java/com/nwalker1208/automute/App.java
@@ -0,0 +1,13 @@
+package com.nwalker1208.automute;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
