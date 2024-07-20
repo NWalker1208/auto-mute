9902871378c5803eb35a5de78d43fe78c5e85dc4
Nathan Walker <nathanwalker1208@gmail.com>
2024-07-19 19:57:11 -0600

    Replace source for default wordlist

--- src/build_wordlist.py
+++ src/build_wordlist.py
@@ -4,7 +4,8 @@ import urllib
 import urllib.request
 from automute import encipher
 
-URL = "https://raw.githubusercontent.com/InnovativeInventor/dict4schools/master/blacklists/blacklist_light.txt"
+#URL = "https://raw.githubusercontent.com/InnovativeInventor/dict4schools/master/blacklists/blacklist_light.txt"
+URL = "https://raw.githubusercontent.com/coffee-and-fun/google-profanity-words/main/data/en.txt"
 
 print("Downloading wordlist...")
 count = 0
@@ -12,6 +13,8 @@ with open("default_wordlist_en.txt", "w") as dest:
   dest.write(f"# Downloaded from {URL} and enciphered.\n")
   with urllib.request.urlopen(URL) as source:
     for line in source:
-      dest.write("\\b" + encipher(line.decode('utf8').rstrip()) + "\\b\n")
-      count += 1
+      word: str = line.decode('utf8').rstrip()
+      if word.isalpha():
+        dest.write("\\b" + encipher(word) + "\\b\n")
+        count += 1
 print(f"Created wordlist with {count} words")
