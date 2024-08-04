6f649bfdf1aa9b6267f75639d206731aa1677394
Nathan Walker <nathanwalker1208@gmail.com>
2024-08-03 18:23:57 -0600

    Move HTML out of source files

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,12 +1,4 @@
 import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <h1>Demo</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-  </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const counter = document.querySelector<HTMLButtonElement>('#counter')!;
+setupCounter(counter);
